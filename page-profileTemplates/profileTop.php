be41999cdd7d0b1259944b8eeb7cdddd5fa0fdc5
kasuyaofcb <dtl_reco@yahoo.co.jp>
2022-01-11 23:21:46 +0700

    プロフィールページ実装

---
+++ page-profileTemplates/profileTop.php
@@ -0,0 +1,75 @@
+<article class="profile">
+    <div class="profileInner">
+        <address class="address">
+            <ul class="addressWrapper">
+                <li class="address__item">
+                    <h1 class="item__name h1Text">
+                        ベジョータ ムチョ
+                    </h1>
+                    <p class="item__tel">
+                        TEL 075-351-5926
+                    </p>
+                    <p class="item__add">
+                        京都市左京区室町仏西入る糸屋町228
+                    </p>
+                    <div class="map__layout">
+                        <figure class="item__map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.2129781978565!2d135.75571631558446!3d35.00137337456242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001089942c28b3b%3A0x7a2f553b8adf4b86!2z44OZ44K444On44O844K_IOODoOODgeODpyDlrqTnlLrku4_lhYnlr7o!5e0!3m2!1sja!2sth!4v1641904542906!5m2!1sja!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" class="map"></iframe>
+                        </figure>
+                    </div>
+                </li>
+                <li class="address__item">
+                    <h1 class="item__name h1Text">
+                        ベジョータ コンチャ
+                    </h1>
+                    <p class="item__tel">
+                        TEL 075-351-5829
+                    </p>
+                    <p class="item__add">
+                        京都市左京区富小路通四条下る徳正寺町418
+                    </p>
+                    <div class="map__layout">
+                        <figure class="item__map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.2126287347946!2d135.755716315512!3d35.00138212368646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001089659285097%3A0x8dbc7cdfb3589c82!2zQkVMTE9UQSBDT05DSEEg44OZ44K444On44O844K_IOOCs-ODs-ODgeODoyDlm5vmnaHlr4zlsI_ot68!5e0!3m2!1sja!2sth!4v1641904731407!5m2!1sja!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" class="map"></iframe>
+                        </figure>
+                    </div>
+                </li>
+                <li class="address__item">
+                    <h1 class="item__name h1Text">
+                        ベジョータ ムアムア
+                    </h1>
+                    <p class="item__tel">
+                        TEL 075-253-0188
+                    </p>
+                    <p class="item__add">
+                        京都市中央区三条通烏丸東入る梅忠町20-1-81
+                    </p>
+                    <div class="map__layout">
+                        <figure class="item__map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3267.9258248969904!2d135.75828851558475!3d35.00856187417451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60010883810d0529%3A0x62e0127842220cad!2z44OZ44K444On44O844K_IOODoOOCouODoOOCoiDng4_kuLjkuInmnaE!5e0!3m2!1sja!2sth!4v1641904821099!5m2!1sja!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" class="map"></iframe>
+                        </figure>
+                    </div>
+                </li>
+                <li class="address__item">
+                    <h1 class="item__name h1Text">
+                        ロティチキン&ジャッキータコス
+                    </h1>
+                    <p class="item__tel">
+                        TEL 075-352-0585
+                    </p>
+                    <p class="item__add">
+                        京都府京都市下京区烏丸東入ル匂天神町642-3
+                    </p>
+                    <div class="map__layout">
+                        <figure class="item__map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.250404002977!2d135.75799731558442!3d35.00043637461294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600108984919fce9%3A0x99e1e635b705887b!2z44Ot44OG44Kj44O8IOODgeOCreODs-OCouODs-ODiSDjgrjjg6Pjg4Pjgq3jg7wg44K_44Kz44K5IOeDj-S4uOmrmOi-uw!5e0!3m2!1sja!2sth!4v1641904968587!5m2!1sja!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" class="map"></iframe>
+                        </figure>
+                    </div>
+                </li>
+            </ul>
+        </address>
+        <p class="profile__title">
+            PROFILE
+        </p>
+    </div>
+</article>
